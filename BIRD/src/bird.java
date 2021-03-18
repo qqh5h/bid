888ca44fafdd574e9e66b6c2d382881cd8ec9fcf
qahwa <qahwa@LAPTOP-DHV75JN1>
2021-03-18 11:25:38 +0300

    inheritance

---
+++ BIRD/src/bird.java
@@ -0,0 +1,20 @@
+
+public class bird {
+
+	
+	public string color;
+	
+	public void fly() {
+		system.out.println("fly name");
+	}
+
+
+public void tweet() {
+	system.out.println("tweet name");
+}
+
+
+
+
+
+}
---
+++ BIRD/src/parot.java
@@ -0,0 +1,14 @@
+
+public class parot {
+
+	
+	public static void main (string[]args) {
+		
+	}
+		public void talk() {
+		
+			bird parot=new bird();
+			
+			System.out.println("bird.tweet()")
+	}
+}
